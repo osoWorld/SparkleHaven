678cb4994603333c759eccde14a922783e834afa
osoWorld <osoworld999@gmail.com>
2024-07-04 01:05:03 +0500

    Settings UI

---
+++ app/src/main/java/com/example/sparklehaven/dashboard/navigation_fragments/profile/classes/adapters/SettingsItemAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.sparklehaven.dashboard.navigation_fragments.profile.classes.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sparklehaven.dashboard.navigation_fragments.home.classes.model.HomeCategoryModel
+import com.example.sparklehaven.databinding.SettingsItemLayoutBinding
+
+class SettingsItemAdapter (private val context: Context) : RecyclerView.Adapter<SettingsItemAdapter.SettingsItemViewHolder>() {
+
+    private var settingsItemList: List<HomeCategoryModel> = listOf()
+    class SettingsItemViewHolder (val binding: SettingsItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SettingsItemViewHolder {
+        return SettingsItemViewHolder(
+            SettingsItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = settingsItemList.size
+
+    override fun onBindViewHolder(holder: SettingsItemViewHolder, position: Int) {
+        holder.binding.apply {
+            settingsItemIcon.setImageResource(settingsItemList[position].icon)
+            settingsItemName.text = settingsItemList[position].title
+        }
+    }
+    fun updateItems(newItems: List<HomeCategoryModel>) {
+        settingsItemList = newItems
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/example/sparklehaven/dashboard/navigation_fragments/profile/classes/view_model/SettingsViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.sparklehaven.dashboard.navigation_fragments.profile.classes.view_model
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.sparklehaven.R
+import com.example.sparklehaven.dashboard.navigation_fragments.home.classes.model.HomeCategoryModel
+
+class SettingsViewModel : ViewModel() {
+    private val _upperSettingItemsList = MutableLiveData<List<HomeCategoryModel>>()
+    val upperSettingListItems: LiveData<List<HomeCategoryModel>> get() = _upperSettingItemsList
+
+    private val _lowerSettingItemsList = MutableLiveData<List<HomeCategoryModel>>()
+    val lowerSettingListItems: LiveData<List<HomeCategoryModel>> get() = _lowerSettingItemsList
+
+    init {
+        Log.d("SettingsViewModel", "Initializing ViewModel")
+        loadUpperSettingsItems()
+        loadLowerSettingsItems()
+    }
+
+    private val upperSettingItems: List<HomeCategoryModel> = listOf(
+        HomeCategoryModel(R.drawable.profile, "Profile"),
+        HomeCategoryModel(R.drawable.message, "Message"),
+        HomeCategoryModel(R.drawable.shopping_bag, "My Order"),
+        HomeCategoryModel(R.drawable.unfav_icon, "Favorite"),
+        HomeCategoryModel(R.drawable.shipping_address, "Shipping Address"),
+        HomeCategoryModel(R.drawable.credit_card, "My Card"),
+        HomeCategoryModel(R.drawable.setting, "Settings"),
+    )
+
+    private val lowerSettingItems: List<HomeCategoryModel> = listOf(
+        HomeCategoryModel(R.drawable.faq, "FAQ"),
+        HomeCategoryModel(R.drawable.privacy_policy, "Privacy Policy"),
+        HomeCategoryModel(R.drawable.rate_star, "Ratings"),
+        HomeCategoryModel(R.drawable.help, "Help"),
+    )
+
+    fun loadUpperSettingsItems() {
+        Log.d("SettingsViewModel", "Loading upper settings items")
+        _upperSettingItemsList.postValue(upperSettingItems)
+    }
+
+    fun loadLowerSettingsItems() {
+        Log.d("SettingsViewModel", "Loading lower settings items")
+        _lowerSettingItemsList.postValue(lowerSettingItems)
+    }
+}

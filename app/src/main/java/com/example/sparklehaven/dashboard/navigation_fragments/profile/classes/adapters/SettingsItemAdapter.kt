03927817ff89111a8ac028e6ecb59507c5e03724
osoWorld <osoworld999@gmail.com>
2024-07-05 20:03:18 +0500

    Profile UI

--- app/src/main/java/com/example/sparklehaven/dashboard/navigation_fragments/profile/classes/adapters/SettingsItemAdapter.kt
+++ app/src/main/java/com/example/sparklehaven/dashboard/navigation_fragments/profile/classes/adapters/SettingsItemAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.sparklehaven.dashboard.navigation_fragments.profile.classes.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sparklehaven.dashboard.navigation_fragments.home.classes.model.HomeCategoryModel
+import com.example.sparklehaven.dashboard.navigation_fragments.profile.activities.ProfileActivity
 import com.example.sparklehaven.databinding.SettingsItemLayoutBinding
 
 class SettingsItemAdapter (private val context: Context) : RecyclerView.Adapter<SettingsItemAdapter.SettingsItemViewHolder>() {
@@ -26,11 +28,19 @@ class SettingsItemAdapter (private val context: Context) : RecyclerView.Adapter<
     override fun getItemCount(): Int = settingsItemList.size
 
     override fun onBindViewHolder(holder: SettingsItemViewHolder, position: Int) {
+        val settingItems = settingsItemList[position]
         holder.binding.apply {
-            settingsItemIcon.setImageResource(settingsItemList[position].icon)
-            settingsItemName.text = settingsItemList[position].title
+            settingsItemIcon.setImageResource(settingItems.icon)
+            settingsItemName.text = settingItems.title
             if (position == settingsItemList.size) {
                 greyUnderline.visibility = View.INVISIBLE
+            }
+        }
+
+        holder.itemView.setOnClickListener {
+            if (settingItems.title == "Profile") {
+                val intent = Intent(context, ProfileActivity::class.java)
+                context.startActivity(intent)
             }
         }
     }
